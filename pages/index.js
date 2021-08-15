dec87f2922d535321896d8c015d73bb5d1d3895d
Victor Radael <victorradael@hotmail.com>
2021-08-14 22:38:17 -0300

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,41 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
+  const date = new Date();
+  const marryDate = new Date("September 10, 2021 00:00:00");
+  var count = ((marryDate - date) / 1000 / 60 / 60 / 24).toFixed(0);
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Carol & Victor</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Carol & Victor </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <iframe
+          src="https://player.twitch.tv/?channel=astronautvr&parent=localhost&output=embed"
+          scrolling="no"
+          height="414"
+          width="896"
+        ></iframe>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {count == 0 ? (
+          <>
+            <h1 className="title space">Chegou o Dia!</h1>
+            <p>às 16:30 h</p>
+          </>
+        ) : (
+          <h1 className="title space">Faltam {count} Dias</h1>
+        )}
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -67,119 +45,38 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          background: #015abe;
+          background: url("https://www.teahub.io/photos/full/247-2474382_abstract-nature-wallpaper-imagem-de-fazenda-papel-de.jpg") -20rem;
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
         }
 
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
-        }
-
-        .title,
-        .description {
+          font-family: "Parisienne", cursive;
+          color: #fff;
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
+        p {
           margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
+          line-height: 1.15;
+          font-size: 2rem;
+          font-family: "Parisienne", cursive;
+          color: #fff;
+          text-align: center;
+          font-weight: bold;
         }
 
-        .logo {
-          height: 1em;
+        .space {
+          padding: 1rem 0 0 0;
         }
 
         @media (max-width: 600px) {
@@ -193,6 +90,7 @@ export default function Home() {
       <style jsx global>{`
         html,
         body {
+          border: 0;
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
@@ -205,5 +103,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
