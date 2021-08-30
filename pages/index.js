beea923d61c48e4cc996257a879e51458ce3e6b7
Victor Radael <victorradael@hotmail.com>
2021-08-30 20:26:25 -0300

    Change Gift Modal

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import { useState } from "react";
 import { MdScreenRotation } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
   const date = new Date();
   const marryDate = new Date("September 10, 2021 00:00:00");
   var count = ((marryDate - date) / 1000 / 60 / 60 / 24).toFixed(0);
@@ -28,20 +30,49 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main>
         <h1 className="title">Carol & Victor </h1>
+        {!modalOpen ? (
+          <iframe
+            src={`https://player.twitch.tv/?channel=astronautvr&parent=${urlParent}&output=embed`}
+            scrolling="no"
+          ></iframe>
+        ) : (
+          <div className="modal">
+            Presentear o casal é uma das tradições mais antigas quando falamos
+            de casamento, e conosco não iria ser diferente... Pelo menos nem
+            tanto...
+            <br /> Não faremos, como de costume, uma lista com utensílios
+            domésticos. Nós decidimos criar uma "poupança do casal" pois estamos
+            planejando viajar assim que a situação mundial melhorar e também
+            para podermos escolher onde melhor aplicar o dinheiro e de que
+            maneira. <br /> Sendo assim, você pode contribuir com nossa poupança
+            da maneira que desejar ! Temos algumas opções para você: <br />
+            1) Chave Pix: <br />
+            CPF: 12619341728 <br />
+            Dados: Carolina M B Legg (Noiva) <br />
+            2) Transferências bancárias Opções:
+            <br />
+            Banco do Brasil, Itaú, Bradesco, Santander, Nubank e Inter. <br />
+            Caso deseje fazer transferência bancaria para um dos bancos citados
+            acima, entre em contato pelo WhatsApp ou ligue para o telefone:{" "}
+            <br />
+            (22)9-9895-6544
+          </div>
+        )}
 
-        <iframe
-          src={`https://player.twitch.tv/?channel=astronautvr&parent=${urlParent}&output=embed`}
-          scrolling="no"
-        ></iframe>
-
-        <div className="icon">
-          <MdScreenRotation size="8rem" />
-          <p>Gire o aparelho</p>
-        </div>
+        {!modalOpen && (
+          <div className="icon">
+            <MdScreenRotation size="8rem" />
+            <p>Gire o aparelho</p>
+          </div>
+        )}
 
         <div className="bottom">
           {count == 0 ? (
@@ -53,16 +84,33 @@ export default function Home() {
             <h1 className="footer space">Faltam {count} Dias</h1>
           )}
 
-          <div
-            className="contactCard whatsIcon"
-            href="/#"
-            onClick={() => openWhatsappChat()}
-            target="blank"
-          >
-            <p className="contact">Gostaria de nos presentear?</p>
-            <p className="contact">Clique aqui!</p>
-          </div>
+          {!modalOpen ? (
+            <div
+              className="contactCard whatsIcon"
+              href="/#"
+              // onClick={() => openWhatsappChat()}
+              onClick={() => setModalOpen(true)}
+              target="blank"
+            >
+              <p className="contact">Gostaria de nos presentear?</p>
+              <p className="contact">Clique aqui!</p>
+            </div>
+          ) : (
+            <div
+              className="contactCard whatsIcon"
+              href="/#"
+              onClick={() => setModalOpen(false)}
+              target="blank"
+            >
+              <p className="contact">Voltar</p>
+            </div>
+          )}
         </div>
+        {modalOpen && (
+          <div className="whatsIconButton" onClick={() => openWhatsappChat()}>
+            <FaWhatsapp size="2rem" color="#fff" />
+          </div>
+        )}
       </main>
 
       <style jsx>{`
@@ -85,6 +133,27 @@ export default function Home() {
 
         .contact {
           font-size: 1.3rem;
+        }
+
+        .modal {
+          display: flex;
+          flex-direction: column;
+          width: 100vw;
+          height: 80vh;
+          font-family: "Roboto", sans-serif !important;
+          font-weight: bold;
+          color: #fff;
+          padding: 2rem;
+          text-align: justify;
+          font-size: 1.3rem;
+          line-height: 1.9rem;
+
+          background: rgba(23, 22, 22, 0.35);
+          box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+          backdrop-filter: blur(9px);
+          -webkit-backdrop-filter: blur(9px);
+          border-radius: 10px;
+          border: 1px solid rgba(255, 255, 255, 0.18);
         }
 
         .bottom {
@@ -140,6 +209,13 @@ export default function Home() {
           text-align: center;
         }
 
+        .whatsIconButton {
+          position: absolute;
+          bottom: 10rem;
+          right: 1rem;
+          cursor: pointer;
+        }
+
         p {
           margin: 0;
           line-height: 1.15;
@@ -184,6 +260,11 @@ export default function Home() {
           .icon {
             display: flex;
           }
+
+          .modal {
+            font-size: 0.7rem;
+            line-height: 1.3rem;
+          }
         }
 
         @media (max-width: 414px) {
@@ -202,6 +283,11 @@ export default function Home() {
           .icon {
             display: flex;
           }
+
+          .modal {
+            font-size: 0.8rem;
+            line-height: 1.3rem;
+          }
         }
 
         @media (max-width: 684px) {
@@ -218,6 +304,12 @@ export default function Home() {
             height: 70vh;
             width: 80vw;
           }
+
+          .modal {
+            font-size: 0.7rem;
+            line-height: 1.3rem;
+            padding: 1rem;
+          }
         }
 
         @media (max-width: 768px) {
@@ -233,6 +325,12 @@ export default function Home() {
           iframe {
             height: 70vh;
             width: 80vw;
+          }
+
+          .modal {
+            font-size: 0.7rem;
+            line-height: 1.3rem;
+            padding: 1rem;
           }
         }
       `}</style>
